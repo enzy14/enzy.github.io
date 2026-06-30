d22e29970dc9b5fe246b0a09b212edb7918af329
enzyping <miano.frecymae0414@gmail.com>
2026-06-30 15:22:31 +0800

    Add files via upload

---
+++ JavaApplication.java
@@ -0,0 +1,14 @@
+
+package javaapplication;
+
+
+public class JavaApplication {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        gui GuiFrame = new gui();
+        GuiFrame.setVisible(true);
+    }
+    
+}
